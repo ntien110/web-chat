5ee30e2bf9723ac6c7c5aba43ede8e4079ddc292
tienlk25 <56721312+tienlk25@users.noreply.github.com>
2019-12-21 17:56:44 +0700

    update index from dev

--- backendServer/apiServer/index.js
+++ backendServer/apiServer/index.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors=require("cors");
+const webSecketServer=require("../websocketServer/server")
+
 const app = express();
+server=require("http").createServer(app)
+const io=require('socket.io')(server);
+
+server.listen(3002);
+
+// io.on("connection", (socket) => {
+//     console.log("connected")
+//     userId = null;
+//     socket.emit("welcome", "wellcome message")
+//     socket.on("return",(data)=>{
+//         console.log(data)
+//     })})
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 app.use((req,res,next)=>{
     console.log(`a ${req.method} request from ${req.host}`)
+    //console.log(req)
     next()
 })
 
-const login = require("./user");
-app.use(login);
-const rooms = require("./rooms");
-app.use(rooms);
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Node.js listening ...');
-});
+webSecketServer(io)
 
-module.exports={app}
+    
+
+const login=require("./user")
+app.use(login)
+
+const rooms=require("./rooms")
+app.use(rooms)
+
+ app.listen(process.env.PORT || 3000, function () {
+     console.log('Node.js listening ...');
+ });
